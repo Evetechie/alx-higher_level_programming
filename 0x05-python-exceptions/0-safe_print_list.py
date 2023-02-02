14c1cd4f92f925d2e2568d88a4b5219581c55a81
Evetechie <muemaevelyn@gmail.com>
2023-02-02 02:31:49 -0800

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         The real number of elements printed.
     """
-    ret = 0
+    sum = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")

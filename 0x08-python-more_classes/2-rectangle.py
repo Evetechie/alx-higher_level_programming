913b0464d936d29cdca418931bb6a065a4c85fb9
Evetechie <muemaevelyn@gmail.com>
2023-02-03 23:14:10 -0800

    more classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
 
         def area(self):
             """Return the area of the Rectangle."""
-            return (self.__width * self.__width)
+            return (self.__width * self.__height)
 
         def perimeter(self):
             """Return the perimeter of the rectangle."""
